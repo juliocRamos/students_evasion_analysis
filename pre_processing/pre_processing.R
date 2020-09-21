3579072ec5940edc4b936196ab7ac3681438c545
Julio Cesar Ramos <ps2.ramos@gmail.com>
2020-09-20 22:11:41 -0300

    [r-script][pre-processing] New code integration and improvements

--- pre_processing/pre_processing.R
+++ pre_processing/pre_processing.R
@@ -5,9 +5,6 @@ library(scales)
 library(data.table)
 library(tidyr)
 
-# TODO: colocar a coluna TOT_REPROVACOES entre as primeiras colunas.
-# da forma que fiz ela fica na última posição.
-
 # Leitura do dataset
 evasao_alunos <- read.csv("datasets/dados_evasao.csv", encoding = "UTF-8")
 
@@ -18,48 +15,6 @@ apply(evasao_alunos, 2, function(x) any(is.na(x)))
 # Filtra apenas a grade superior a 2015
 evasao_filtrado <- filter(evasao_alunos, GRADE_CORRENTE >= "2015")
 
-tmp_1 <- evasao_filtrado
-
-# Tranformando os Status da coluna DSC_STATUS_MAT em numérico onde:
-# 0 = Não Cursou a disciplina(*)
-# 1 = APROVADO
-# 2 = REPROVADO POR NOTA
-# 3 = REPROVADO POR FREQ.
-# 4 = REPROVADO NOTA & FREQ.
-# 5 = TEVE REPROVAÇÃO(ÕES) E FOI APROVADO POSTERIORMENTE(*)
-# (*) Serão inseridos nas próximas linhas os valores
-tmp_1$DSC_STATUS_MAT[which(tmp_1$DSC_STATUS_MAT == "APROVADO")] <- as.numeric(as.character(1))
-tmp_1$DSC_STATUS_MAT[which(tmp_1$DSC_STATUS_MAT == "REPROVADO POR NOTA")] <- as.numeric(as.character(2))
-tmp_1$DSC_STATUS_MAT[which(tmp_1$DSC_STATUS_MAT == "REPROVADO POR FREQ.")] <- as.numeric(as.character(3))
-tmp_1$DSC_STATUS_MAT[which(tmp_1$DSC_STATUS_MAT == "REPROVADO NOTA & FREQ.")] <- as.numeric(as.character(4))
-
-# Faz o transpose entre duas colunas (COD_MATERI e DSC_STATUS_MAT), fazendo uma coluna de disciplina em seus
-# respectivos status para cada aluno e fazendo o merge de RAs (por aluno).
-# Obs.: values_fill = 0 ---> Valores null corresponde a alunos que não cursaram a disciplina,
-# atribuindo o valor 0 para esses casos;
-# values_fn = function(x) -----> Função criada para tratar dados duplicados (Alunos que possuem duas aprovações
-# ou que foram reprovados e aprovados na ultima ocorrencia).
-tmp_1 <- pivot_wider(tmp_1, 
-                     id_cols = RA, 
-                     names_from = COD_MATERI, 
-                     values_from = DSC_STATUS_MAT, 
-                     values_fill = 0, 
-                     values_fn = function(x){
-                        if(length(x) > 1){
-                          last_value <- tail(x, n=1)
-                          first_value <- head(x, n=1)
-                          if (first_value == "1") {
-                            return(1)
-                          } else if(last_value %in% c("2", "3", "4")) {
-                            return(as.numeric(as.character(last_value)))
-                          } else {
-                            return(5)
-                          }
-                        } else {
-                          return(as.numeric(as.character(x)))
-                        }
-                      })
-
 # Faco o replace de , por .
 evasao_filtrado$NOTA_MEDIA <- gsub(",", '.', evasao_filtrado$NOTA_MEDIA, fixed =T)
 evasao_filtrado$PONTUACAO_PS <- gsub(",", '.', evasao_filtrado$PONTUACAO_PS, fixed =T)
@@ -67,6 +22,19 @@ evasao_filtrado$PONTUACAO_PS <- gsub(",", '.', evasao_filtrado$PONTUACAO_PS, fix
 # Converto as notas para numerico
 evasao_filtrado$NOTA_MEDIA <- as.numeric(as.character(evasao_filtrado$NOTA_MEDIA))
 evasao_filtrado$PONTUACAO_PS <- as.numeric(as.character(evasao_filtrado$PONTUACAO_PS))
+
+# Converte 'S' ou 'N' em booleano 0 ou 1
+colunas_convertidas <- lapply(evasao_filtrado, function(x){
+  gsub("S", 1, x)
+})
+
+colunas_convertidas <- lapply(colunas_convertidas, function(x){
+  gsub("N", 0, x)
+})
+
+evasao_filtrado$EVADIDO <- colunas_convertidas$EVADIDO
+evasao_filtrado$RESID_ARARAS <- colunas_convertidas$RESID_ARARAS
+evasao_filtrado$BOLSISTA <- colunas_convertidas$BOLSISTA
 
 # Unifica o nome de algumas colunas em uma nova, SHA5795_APROVADO
 evasao_filtrado <- evasao_filtrado %>%
@@ -86,7 +54,7 @@ valuesToColumnMean <- function(df) {
 evasao_filtrado <- valuesToColumnMean(evasao_filtrado)
 
 # Criando dataframe temporario (calculo da nota media) 
-tmp <- evasao_filtrado
+tmp_calc_media <- evasao_filtrado
 
 # Preciso dropar algumas colunas e possivelmente adicionar novamente depois
 # para fazer o reshape
@@ -104,19 +72,18 @@ evasao_filtrado <- reshape(data=evasao_filtrado,idvar="RA",
                            direction="wide",
                            sep = "_")
 
-# Alterando as notas das disciplinas pela media geral de cada aluno 
-evasao_filtrado$NOTA_MEDIA <- round(tapply(tmp$NOTA_MEDIA, tmp$RA, mean), 2)
+# Altera as NOTA_MEDIA de cada disciplina por uma NOTA_MEDIA geral
+evasao_filtrado$NOTA_MEDIA <- round(tapply(
+  tmp_calc_media$NOTA_MEDIA, tmp_calc_media$RA, mean), 2)
 
+# Altera as PONTUACAO_PS para uma nota PONTUACAO_PS geral
+evasao_filtrado$PONTUACAO_PS <- round(tapply(
+  tmp_calc_media$PONTUACAO_PS, tmp_calc_media$RA, mean), 2)
 
 # Substituir todos os APROVADOS ou REPROVADOS por 1
 evasao_filtrado <- sapply(evasao_filtrado, function(x) {
   x <- gsub("^.*(APROVADO|REPROVADO).*$", 1, x)
 }) %>% as.data.frame()
-
-# Substitui todos os NAN por 0, o que indicando que o aluno nao cursou
-# ou nao teve o preenchimento do registro informado.
-evasao_filtrado[is.na(evasao_filtrado)] <- 0
-
 
 # Conta as reprovacoes por aluno
 count_reprovacoes <- filter(evasao_alunos, grepl("REPROVADO", DSC_STATUS_MAT)
@@ -126,13 +93,13 @@ count_reprovacoes <- filter(evasao_alunos, grepl("REPROVADO", DSC_STATUS_MAT)
   filter(n >= 1) %>%
   unite(merged_rows, c(RA, n), sep = ", ") 
 
+# Cria a coluna TOT_REPROVACOES
+evasao_filtrado <- evasao_filtrado %>%
+  mutate(TOT_REPROVACOES = 0, .after=PONTUACAO_PS)
 
-# Cria a coluna TOT_REPROVACOES (cai em última posição)
-evasao_filtrado["TOT_REPROVACOES"] <- 0
-
+# Processa os alunos linha a linha para substituir apenas os dos índices corretos
 criarColunasTotReprovacoes <- function(d1, d2) {
 
-  # Processo os alunos linha a linha para substituir apenas os dos índices corretos
   for (x in d1){
     for (i in 1:nrow(d1)) {
       ra <- as.character(strsplit(x, split = ",")[[i]][[1]])
@@ -150,9 +117,62 @@ criarColunasTotReprovacoes <- function(d1, d2) {
 # Aplica as reprovacoes para os alunos adequados
 evasao_filtrado <- criarColunasTotReprovacoes(count_reprovacoes, evasao_filtrado)
 
-# Exporta o dataset final
-write.csv(evasao_filtrado, file = "pre_processed_analysis.csv")
+materias_por_aluno <- filter(evasao_alunos, GRADE_CORRENTE >= "2015")
 
+# Tranformando os Status da coluna DSC_STATUS_MAT em numérico onde:
+# 0 = Não Cursou a disciplina(*)
+# 1 = APROVADO
+# 2 = REPROVADO POR NOTA
+# 3 = REPROVADO POR FREQ.
+# 4 = REPROVADO NOTA & FREQ.
+# 5 = TEVE REPROVAÇÃO(ÕES) E FOI APROVADO POSTERIORMENTE(*)
+# (*) Serão inseridos nas próximas linhas os valores
+materias_por_aluno$DSC_STATUS_MAT[
+  which(materias_por_aluno$DSC_STATUS_MAT == "APROVADO")] <- as.numeric(as.character(1))
+
+materias_por_aluno$DSC_STATUS_MAT[
+  which(materias_por_aluno$DSC_STATUS_MAT == "REPROVADO POR NOTA")] <- as.numeric(as.character(2))
+
+materias_por_aluno$DSC_STATUS_MAT[
+  which(materias_por_aluno$DSC_STATUS_MAT == "REPROVADO POR FREQ.")] <- as.numeric(as.character(3))
+
+materias_por_aluno$DSC_STATUS_MAT[
+  which(materias_por_aluno$DSC_STATUS_MAT == "REPROVADO NOTA & FREQ.")] <- as.numeric(as.character(4))
+
+# Faz o transpose entre as colunas COD_MATERI e DSC_STATUS_MAT, criando uma coluna
+# para cada status e para cada aluno, fazendo o merge de RAs (por aluno).
+# values_fill = 0 (atribui-se 0 para alunos que não fizeram a matéria)
+# values_fn = function(x) (Tratar dados duplicados (Alunos que possuem duas
+# aprovações ou que foram reprovados e aprovados na ultima ocorrencia).
+materias_por_aluno <- pivot_wider(materias_por_aluno, 
+                                id_cols = RA, 
+                                names_from = COD_MATERI,
+                                values_from = DSC_STATUS_MAT,
+                                values_fill = 0,
+                                values_fn = function(x){
+                                  if(length(x) > 1){
+                                    last_value <- tail(x, n=1)
+                                    first_value <- head(x, n=1)
+                                    if (first_value == "1") {
+                                      return(1)
+                                    } else if(last_value %in% c("2", "3", "4")) {
+                                      return(as.numeric(as.character(last_value)))
+                                    } else {
+                                      return(5)
+                                    }
+                                  } else {
+                                    return(as.numeric(as.character(x)))
+                                  }
+                                })
+
+# Filtra as colunas 1 a 8
+evasao_filtrado <- evasao_filtrado %>% select(1:8)
+
+# Mergeia os dataframes por RA para adicionar as colunas de disciplina
+evasao_filtrado <-merge(evasao_filtrado, materias_por_aluno, by = "RA")
+
+# Exporta o dataframe final.
+write.csv(evasao_filtrado, file = "pre_processed_analysis.csv")
 
 
 
