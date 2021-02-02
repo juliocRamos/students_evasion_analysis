2328d85e98aeb89304163e8e7b5717059115bede
dlpires <dlpires@outlook.com>
2021-02-01 23:17:01 -0300

    [python][model-training] Decision Tree - GridSearch and Kfold Implementation + Export CSV tests

--- train_models/utils/model_utils.py
+++ train_models/utils/model_utils.py
@@ -7,6 +7,9 @@ Created on Fri Jan 29 21:10:08 2021
 
 from datetime import datetime
 from time import sleep
+from sklearn import tree
+import pandas as pd
+import graphviz
 import os
 
 # generate a standard output for model prediction results
@@ -25,28 +28,51 @@ def generate_output(all_predictions):
 
                 # TODO: add auc and precision_score, K value for epoch
                 # TODO: add csv, if possible
-                if (all_predictions[i]["model_algorithm"] == "knn"):
-                    file.write("=================RESULTS EPOCH "+str(i)+"==========================\n")
-                    file.write("Best K: " + str(all_predictions[i]["epoch_best_k"])+"\n\n")
-                    file.write(str(all_predictions[i]["confusion_matrix"])+"\n\n")
-                    file.write(str(all_predictions[i]["classification_report"])+"\n\n")
-                    file.write("Accuracy: " + str(all_predictions[i]["model_accuracy"])+"\n\n")
-                    file.write("Epoch params: " + str(all_predictions[i]["epoch_params"])+"\n\n")
-                    file.write("====================END OF EPOCH==========================\n\n")
-                    sleep(1)
-                elif(all_predictions[i]["model_algorithm"] == "svm"):
-                    file.write("=================RESULTS EPOCH "+str(i)+"==========================\n")
-                    file.write(str(all_predictions[i]["confusion_matrix"])+"\n\n")
-                    file.write(str(all_predictions[i]["classification_report"])+"\n\n")
-                    file.write("Accuracy: " + str(all_predictions[i]["model_accuracy"])+"\n\n")
-                    file.write("Epoch params: " + str(all_predictions[i]["epoch_params"])+"\n\n")
-                    file.write("====================END OF EPOCH==========================\n\n")
-                    sleep(1)
+                file.write("=================RESULTS EPOCH "+str(i)+"==========================\n")
+                file.write(str(all_predictions[i]["confusion_matrix"])+"\n\n")
+                file.write(str(all_predictions[i]["classification_report"])+"\n\n")
+                file.write("Accuracy: " + str(all_predictions[i]["model_accuracy"])+"\n\n")
+                file.write("Epoch params: " + str(all_predictions[i]["epoch_params"])+"\n\n")
+                file.write("====================END OF EPOCH==========================\n\n")
+                sleep(1)
         except Exception as ex:
             raise(ex)
         finally:
             file.close()
 
+def generate_output_csv(n_epoch, all_predictions):
+    try:
+        ## Create file name
+        logfile = "predictions_results.csv"
+
+        ## Verify if all_predictions variable has content
+        if len(all_predictions) == 0:
+            print("Nothing to write in file!!")
+        
+        if (os.path.exists(logfile)):
+            dt = pd.read_csv(logfile)
+        else:
+            dt = pd.DataFrame(columns = ["epoch","model_accuracy",
+                                         "f1_score","precision_score",
+                                         "roc_score","epoch_best_params"])
+        ## create Dict to output data
+        for i in range(len(all_predictions)):
+            dt2 = {
+                "epoch": n_epoch,
+                "model_accuracy": all_predictions[i]["model_accuracy"],
+                "f1_score": all_predictions[i]["f1_score"],
+                "precision_score": all_predictions[i]["precision_score"],
+                "roc_score": all_predictions[i]["roc_score"],
+                "epoch_best_params": all_predictions[i]["epoch_params"]
+            }
+        dt = dt.append(dt2, ignore_index=True)
+        
+        ## Write CSV File
+        dt.to_csv(logfile, index = False)
+        
+    except Exception as ex:
+        raise(ex)        
+            
 # Get best K (TEST)
 def find_best_k(n_tests, X_train, X_test, y_train, y_test):
     errors = []
@@ -71,3 +97,20 @@ def find_best_k(n_tests, X_train, X_test, y_train, y_test):
     plt.xlabel('K Value')
     plt.ylabel('Mean Error')
     return(min(all_key_errors, key=lambda x: x[1])[0])
+    
+def plot_tree(clf, df):
+    feature_names = df.drop("EVADIDO", axis = 1)
+    feature_names = feature_names.columns
+    class_names = ["EVADIDO","NÃO EVADIDO"]
+
+    dot_data = tree.export_graphviz(clf, out_file = None,
+                    feature_names=feature_names,
+                    class_names=class_names,
+                    filled=True, 
+                    rounded=True,
+                    special_characters=True,
+                    precision=3)
+
+    graph = graphviz.Source(dot_data)
+    graph.format = "png"
+    graph.render('dtree_render',view=True)
