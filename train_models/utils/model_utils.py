d49ff0ff808c6654ccd0899e590977e4d7fb99c7
Julio Cesar Ramos <ps2.ramos@gmail.com>
2021-02-16 01:48:19 -0300

    [python-script][model-training] General fixes for training modules

--- train_models/utils/model_utils.py
+++ train_models/utils/model_utils.py
@@ -13,25 +13,31 @@ import graphviz
 import os
 
 # generate a standard output for model prediction results
-def generate_output(all_predictions):
+def generate_output(all_predictions, logdir = "", model = ""):
     for i in range(len(all_predictions)):
         try:
-            logdir = "predictions_" + datetime.now().strftime("%Y%m%d-%H%M%S")
-            os.makedirs(logdir)
-
             if (os.path.exists(logdir)):
-                predictions_path = ""+logdir+"/prediction_summary.txt"
-                file = open(predictions_path, 'w')
+                predictions_dirs = "predictions_" + datetime.now().strftime("%Y%m%d-%H%M%S")
+                full_path = os.path.join(logdir, model, predictions_dirs)
+                if os.path.exists(full_path) != True:
+                    os.makedirs(full_path)
+
+                prediciton_file = full_path + "/prediction_summary.txt"
+                file = open(prediciton_file, 'w')
 
                 if len(all_predictions) == 0:
                     print("Nothing to write in file!!")
 
-                # TODO: add auc and precision_score, K value for epoch
-                # TODO: add csv, if possible
                 file.write("=================RESULTS EPOCH "+str(i)+"==========================\n")
                 file.write(str(all_predictions[i]["confusion_matrix"])+"\n\n")
                 file.write(str(all_predictions[i]["classification_report"])+"\n\n")
                 file.write("Accuracy: " + str(all_predictions[i]["model_accuracy"])+"\n\n")
+                file.write("F1 score: " + str(all_predictions[i]["f1_score"])+"\n\n")
+                file.write("Precision: " + str(all_predictions[i]["precision_score"])+"\n\n")
+                file.write("ROC: " + str(all_predictions[i]["roc_score"])+"\n\n")
+                file.write("Max error: " + str(all_predictions[i]["max_error"])+"\n\n")
+                file.write("Mean squared err.: " + str(all_predictions[i]["mean_squared_error"])+"\n\n")
+                file.write("Mean abs. err.: " + str(all_predictions[i]["mean_absolute_error"])+"\n\n")
                 file.write("Epoch params: " + str(all_predictions[i]["epoch_params"])+"\n\n")
                 file.write("====================END OF EPOCH==========================\n\n")
                 sleep(1)
@@ -40,21 +46,24 @@ def generate_output(all_predictions):
         finally:
             file.close()
 
-def generate_output_csv(all_predictions):
+def generate_output_csv(all_predictions, logdir = "", model = ""):
     try:
         ## Create file name
-        logfile = "predictions_results.csv"
+        file_name = "predictions_results_" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".csv"
+        results_path = os.path.join(logdir, model, file_name)
 
         ## Verify if all_predictions variable has content
         if len(all_predictions) == 0:
             print("Nothing to write in file!!")
         
-        if (os.path.exists(logfile)):
-            dt = pd.read_csv(logfile)
+        if (os.path.exists(results_path)):
+            dt = pd.read_csv(results_path)
         else:
-            dt = pd.DataFrame(columns = ["model_accuracy",
-                                         "f1_score","precision_score",
-                                         "roc_score","epoch_best_params"])
+            dt = pd.DataFrame(columns = ["model_accuracy", "f1_score",
+                                         "precision_score", "roc_score",
+                                         "recall_score", "max_error",
+                                         "mean_squared_error", "mean_absolute_error",
+                                         "epoch_best_params"])
         
         
         ## insert in Dict the output data
@@ -65,13 +74,17 @@ def generate_output_csv(all_predictions):
                 "f1_score": all_predictions[i]["f1_score"],
                 "precision_score": all_predictions[i]["precision_score"],
                 "roc_score": all_predictions[i]["roc_score"],
+                "recall_score": all_predictions[i]["recall_score"],
+                "max_error": all_predictions[i]["max_error"],
+                "mean_squared_error": all_predictions[i]["mean_squared_error"],
+                "mean_absolute_error": all_predictions[i]["mean_absolute_error"],
                 "epoch_best_params": all_predictions[i]["epoch_params"]
             }
             ## Append dict in the dataframe
             dt = dt.append(dt2, ignore_index=True)
         
             ## Write CSV File
-            dt.to_csv(logfile, index = False)
+            dt.to_csv(results_path, index = False)
         
     except Exception as ex:
         raise(ex)        
